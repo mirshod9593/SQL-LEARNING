c1299621cee7de4d34a494f3d728a743535927bc
mirshod9593 <abdiraxmonovmirshodbek@gmail.com>
2025-08-19 15:21:00 +0500

    Create lesson6hw.sql

---
+++ lesson-6/homework/lesson6hw.sql
@@ -0,0 +1,18 @@
+1.  select distinct col1, col2 from InputTbl;
+
+2.	select * from TestMultipleZero
+	where A>0 or B> 0 OR C>0 OR D>0
+3. SELECT * FROM section1
+    WHERE ID%2=1
+4.   SELECT MIN(ID) FROM section1;
+5.   SELECT MAX(ID) FROM section1;
+6.  SELECT * FROM section1
+  WHERE NAME LIKE 'A%'
+ 
+
+7.  SELECT * FROM ProductCodes
+    WHERE CODE LIKE '%\_%' ESCAPE '\';
+
+
+
+
